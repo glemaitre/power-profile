784b4c2cc67fcaaf11e9891d29ad5133a356f991
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-27 07:35:58 +0100

    Change the main file to work with the new version of fitparse

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@
 # print its data records.
 
 
-from fitparse import Activity
+from fitparse import FitFile
 
-activity = Activity("/home/cedric/Documents/Code/Sport/power-profile/data/user_1/2015/2015-08-09-13-14-05.fit")
+activity = FitFile("../data/user_1/2015/2015-08-09-13-14-05.fit")
 activity.parse()
 
 # Records of type 'record' (I know, confusing) are the entries in an
 # activity file that represent actual data points in your workout.
-records = activity.get_records_by_type('record')
+records = list(activity.get_messages(name='record'))
 current_record_number = 0
 
 for record in records:
@@ -19,18 +19,8 @@ for record in records:
     current_record_number += 1
     print (" Record #%d " % current_record_number).center(40, '-')
 
-    # Get the list of valid fields on this record
-    valid_field_names = record.get_valid_field_names()
-
-    for field_name in valid_field_names:
+    for field in record:
         # Get the data and units for the field
-        field_data = record.get_data(field_name)
-        field_units = record.get_units(field_name)
-        
-        # Print what we've got!
-        if field_units:
-            print " * %s: %s %s" % (field_name, field_data, field_units)
-        else:
-            print " * %s: %s" % (field_name, field_data)
-            
+        print (field.name, field.value, field.units)
+
     print
