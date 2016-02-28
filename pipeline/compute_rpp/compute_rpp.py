49edf546a964eac10d50a79632c55f5d33c9a522
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-28 19:09:12 +0100

    Create a pipeline to extract and save the profile

---
+++ pipeline/compute_rpp/compute_rpp.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import numpy as np
+
+from skcycling.utils import load_power_from_fit
+from skcycling.power_profile import Rpp
+
+# The first input argument corresponding to the data path
+data_path = sys.argv[1]
+
+# The second input argument is the storage directory
+storage_path = sys.argv[2]
+
+# We can create a list of all the *.fit files present inside that directory
+# Create a list with the files to considered
+filenames = []
+for root, dirs, files in os.walk(data_path):
+    for file in files:
+        if file.endswith('.fit'):
+            filenames.append(os.path.join(root, file))
+
+max_duration_rpp = 300
+rpp_rider = Rpp(max_duration_rpp=max_duration_rpp)
+# Open each file and fit
+for filename in filenames:
+    # Open the file
+    power_ride = load_power_from_fit(filename)
+    # Fit the ride
+    rpp_rider.fit(power_ride)
+
+# Store the data somewhere
+np.save(os.path.join(storage_path, 'profile.npy'), rpp_rider.rpp_)
