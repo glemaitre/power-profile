369e3d0d6797e2ba1bf4d6a3a23c3771ab97348e
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-28 09:36:33 +0100

    Address syntax error in the class rpp

--- src/scikit-cycling/skcycling/power_profile/rpp.py
+++ src/scikit-cycling/skcycling/power_profile/rpp.py
@@ -42,7 +42,8 @@ class Rpp(object):
 
             return True
 
-    def _check_X(X):
+    @classmethod
+    def _check_X(cls, X):
         """ Private function helper to check if X is of proper size
 
         Parameters
@@ -72,7 +73,7 @@ class Rpp(object):
         X = self._check_X(X)
 
         # Make a partial fitting of the current data
-        return self.partial_fit(self, X)
+        return self.partial_fit(X)
 
 
     def partial_fit(self, X, refit=False):
@@ -88,7 +89,7 @@ class Rpp(object):
         X = self._check_X(X)
 
         # Call the partial fitting
-        return self._partial_fit(self, X, refit=refit)
+        return self._partial_fit(X, refit=refit)
 
 
     def _partial_fit(self, X, refit=False):
@@ -119,14 +120,13 @@ class Rpp(object):
         else:
             # What to do if it was yet another call
             # Compute the rider power-profile for the given X
-            self.rpp = self.compute_ride_rpp(X)
+            self.rpp = self._compute_ride_rpp(X)
             # Update the best rider power-profile
             self._update_rpp()
 
         return self
 
 
-    @staticmethod
     def _compute_ride_rpp(self, X):
         """ Compute the rider power-profile
 
@@ -142,7 +142,7 @@ class Rpp(object):
         rpp = np.zeros(60 * self.max_duration_rpp_)
 
         # For each duration in the rpp
-        for idx_t_rpp, t_rpp in enumerate(rpp):
+        for idx_t_rpp in range(rpp.size):
             # Slice the data such that we can compute efficiently the mean later
             t_crop = np.array([X[i:-idx_t_rpp + i:]
                                 for i in range(idx_t_rpp)])
@@ -153,7 +153,7 @@ class Rpp(object):
                 # Compute the mean for each of these samples
                 t_crop_mean = np.mean(t_crop, axis=0)
                 # Keep the best to store as rpp
-                t_rpp = np.max(t_crop_mean)
+                rpp[idx_t_rpp] = np.max(t_crop_mean)
 
         return rpp
 
@@ -162,11 +162,17 @@ class Rpp(object):
         """ Update the rider power-profile
         """
 
+        # Create a local copy of the best rpp
+        b_rpp = self.rpp_.copy()
+
         # We have to compare the ride rpp with the best rpp
-        for t_rpp, t_best_rpp in zip(self.rpp, self.rpp_):
+        for idx_rpp, (t_rpp, t_best_rpp) in enumerate(zip(self.rpp, self.rpp_)):
             # Update the best rpp in case the power is greater
             if t_rpp > t_best_rpp:
-                t_best_rpp = t_rpp
+                b_rpp[idx_rpp] = t_rpp
+
+        # Apply the copy
+        self.rpp_ = b_rpp.copy()
 
         # In case that current rpp has an higher duration
         # we can append the value
