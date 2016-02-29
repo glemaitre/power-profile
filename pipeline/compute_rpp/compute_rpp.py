c1188205570263f163f944e6d5d2241fd68e922e
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-29 07:37:47 +0100

    Compute the profile for 2015 of user_1

--- pipeline/compute_rpp/compute_rpp.py
+++ pipeline/compute_rpp/compute_rpp.py
@@ -19,7 +19,7 @@ for root, dirs, files in os.walk(data_path):
         if file.endswith('.fit'):
             filenames.append(os.path.join(root, file))
 
-max_duration_rpp = 300
+max_duration_rpp = 30
 rpp_rider = Rpp(max_duration_rpp=max_duration_rpp)
 # Open each file and fit
 for idx_file, filename in enumerate(filenames):
--- script/compute_rpp/compute_rpp.py
+++ script/compute_rpp/compute_rpp.py
@@ -1,1 +1,1 @@
-python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_1/2014 ../../data/user_1/profile/2014
+python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_1/2015 ../../data/user_1/profile/2015
