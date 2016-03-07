ea57668023e84774c30466cd92bce2dbb9085af9
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-03-07 17:19:08 +0100

    Modify the file to take care about the whole profile

--- script/compute_rpp/load_rpp.py
+++ script/compute_rpp/load_rpp.py
@@ -20,7 +20,7 @@ plt.plot(np.linspace(0, rider_rpp.max_duration_rpp_, rider_rpp.rpp_.size),
 
 # Resample the data as the SRM
 ts = np.array([0.016, 0.083, 0.5, 1, 3, 3.5, 4, 4.5, 5, 5.5,
-               6, 6.5, 7, 10, 20, 30])
+               6, 6.5, 7, 10, 20, 30, 45, 60, 120, 180, 240])
 plt.plot(ts, rider_rpp.resampling_rpp(ts, normalized=True))
 
 # Show the plot
@@ -28,7 +28,8 @@ plt.show()
 
 # Fit the model from Pinot et al.
 ts_reg = np.array([4, 4.5, 5, 5.5,
-               6, 6.5, 7, 10, 20, 30])
+                   6, 6.5, 7, 10, 20, 30,
+                   45, 60, 120, 180, 240])
 slope, intercept, std_err = rider_rpp.aerobic_meta_model(ts=ts_reg,
                                                          normalized=True,
                                                          method='lsq')
