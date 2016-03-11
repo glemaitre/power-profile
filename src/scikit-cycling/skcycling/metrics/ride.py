6c981535257054f2101529a60e7d09e395016ac3
Cedric Lemaitre <c.lemaitre58@gmail.com>
2016-03-11 22:30:44 +0100

    metric to test

--- src/scikit-cycling/skcycling/metrics/ride.py
+++ src/scikit-cycling/skcycling/metrics/ride.py
@@ -8,16 +8,18 @@ the lower the better
 """
 
 from ..utils.checker import _check_X
+from ..utils.running_average import _moving_average
+import numpy as np
 
-
-def normalized_power_score(X):
+def normalized_power_score(X, pma):
     """ Compute the normalized power for a given ride
 
     Parameters
     ----------
     X : array-like, shape (n_samples, )
         Array containing the power intensities for a ride.
-
+    pma : double 
+        Value 
     Returns
     -------
     score : float
@@ -26,8 +28,20 @@ def normalized_power_score(X):
 
     # Check the conformity of X
     X = _check_X(X)
+    x = _moving_average(X,n=30)
+    
+    # Removing value < 35% PMA
+    
+    arr = np.array([[-1, 5], [1, 1], [3, 11], [-4, 20], [2, 9]])
+    X[~(X[:] < 0.35*pma)]
+    
+    X = np.power(X,4)
+    X_mean = np.mean(X)
+    
+    np = _iroot(X_mean,4)
+    
 
-    return
+    return np
 
 
 def intensity_factor_ftp_score(X, ftp):
@@ -51,8 +65,10 @@ def intensity_factor_ftp_score(X, ftp):
     X = _check_X(X)
 
     # Compute the resulting IF
+    np = normalized_power_score(X)
 
-    return
+    if_score = np/ftp 
+    return if_score 
 
 
 def intensity_factor_mpa_score(X, mpa):
@@ -79,7 +95,9 @@ def intensity_factor_mpa_score(X, mpa):
 
     # Compute the resulting IF
 
-    return
+    np = normalized_power_score(X)
+    ftp = 0.76 * mpa  
+    return intensity_factor_ftp_score(X, ftp)
 
 
 def training_stress_ftp_score(X, ftp):
@@ -103,6 +121,12 @@ def training_stress_ftp_score(X, ftp):
     # Check the conformity of X
     X = _check_X(X)
 
+    IF_val = intensity_factor_ftp_score(X,ftp)
+
+
+    TSS = (np.size(X)*np.power(IF_val, 2))(3600)
+
+
     return
 
 
@@ -125,5 +149,19 @@ def training_stress_mpa_score(X, mpa):
 
     # Check the conformity of X
     X = _check_X(X)
+    
+    ftp = 0.76*mpa 
 
-    return
+
+
+    return training_stress_ftp_score(X, ftp)
+
+    def _iroot(k, n):
+        u, s = n, n+1
+        while u < s:
+            s = u
+            t = (k-1) * s + n // pow(s, k-1)
+            u = t // k
+        return s
+
+
