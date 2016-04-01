44f841da29e0bbe7d9401de7b697aa57e2c7295d
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-04-01 18:31:11 +0200

    Add the profile of user_5

--- script/compute_rpp/compute_rpp.sh
+++ script/compute_rpp/compute_rpp.sh
@@ -1,9 +1,11 @@
-python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_1/2014 ../../data/user_1/profile/2014
+# python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_1/2014 ../../data/user_1/profile/2014
 
-python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2012 ../../data/user_2/profile/2012
+# python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2012 ../../data/user_2/profile/2012
 
-python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2013 ../../data/user_2/profile/2013
+# python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2013 ../../data/user_2/profile/2013
 
-python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2014 ../../data/user_2/profile/2014
+# python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2014 ../../data/user_2/profile/2014
 
-python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2015 ../../data/user_2/profile/2015
+# python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_2/2015 ../../data/user_2/profile/2015
+
+python ../../pipeline/compute_rpp/compute_rpp.py ../../data/user_5/2015 ../../data/user_5/profile/2015
